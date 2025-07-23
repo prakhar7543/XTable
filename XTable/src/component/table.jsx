679585428aa9e1323aeb0d7de15665d7f143958e
Your Name <you@example.com>
2025-07-24 02:43:39 +0530

    add

--- XTable/src/component/table.jsx
+++ XTable/src/component/table.jsx
@@ -1,56 +1,88 @@
-import './table.css';
-import React from 'react';
+import "./table.css";
+import React, { useEffect, useState } from "react";
 
+let dummyData = [
+  { date: "2022-09-01", views: 100, article: "Article 1" },
 
-let dummyData = 
-[
+  { date: "2023-09-01", views: 100, article: "Article 1" },
 
-    { date: "2022-09-01", views: 100, article: "Article 1" },
+  { date: "2023-09-02", views: 150, article: "Article 2" },
 
-    { date: "2023-09-01", views: 100, article: "Article 1" },
+  { date: "2023-09-02", views: 120, article: "Article 3" },
 
-    { date: "2023-09-02", views: 150, article: "Article 2" },
+  { date: "2020-09-03", views: 200, article: "Article 4" },
+];
 
-    { date: "2023-09-02", views: 120, article: "Article 3" },
+export default function Table() {
+  let [tableData, setTableData] = useState([]);
 
-    { date: "2020-09-03", views: 200, article: "Article 4" }
+  useEffect(() => {
+    setTableData(dummyData);
+    // console.log(tableData);
+  }, []);
 
-]
+  let handleDateClick = () => {
+    let sortedData = [...tableData].sort(
+      (a, b) => {
+        let dateDiff = new Date(b.date) - new Date(a.date)
 
-export default function Table(){
+        if(dateDiff === 0){
+            return b.views - a.views;
+        }
 
+        return dateDiff;
+    });
+    setTableData(sortedData);
+    console.log(sortedData);
+  };
 
-    return (
-        <div className='container'>
-            <div className='tableContainer'>
-                <h1>Date and Views Table</h1>
-                <div className='btn'>
-                    <button>Sort by Date</button>
-                    <button>Sort by Views</button>
-                </div>
-                <div className='table'>
-                    <table>
-                        <tr className='dataHeader'>
-                            <th>Date</th>
-                            <th>Views</th>
-                            <th>Article</th>
-                        </tr>
+  let handleViewsClick = () => {
+    let sortedViews = [...tableData].sort((a, b) => {
+        let viewsDiff = b.views - a.views;
 
-                    {dummyData && dummyData.map((data, index) => (
-                        <div key={index}>
-                            <tr className='dataRows'>
-                                <td className='date'>{data.date}</td>
-                                <td className='views'>{data.views}</td>
-                                <td className='article'>{data.article}</td>
-                            </tr>
-                           
-                        </div>
-                    ))
-                    }
-                    </table>
+        if(viewsDiff === 0){
+            return new Date(b.date) - new Date(a.date);
+        }
 
-                </div>
-            </div>
+        return viewsDiff;
+    })
+
+    setTableData(sortedViews);
+  };
+
+  return (
+    <div className="container">
+      <div className="tableContainer">
+        <h1>Date and Views Table</h1>
+        <div className="btn">
+          <button onClick={handleDateClick}>Sort by Date</button>
+          <button onClick={handleViewsClick}>Sort by Views</button>
         </div>
-    )
+        <div >
+          <table className="table">
+            <thead>
+
+            <tr className="dataHeader">
+              <th>Date</th>
+              <th>Views</th>
+              <th>Article</th>
+            </tr>
+            </thead>
+
+            <tbody>
+
+            {tableData &&
+              tableData.map((data, index) => (
+                <tr className="dataRows" key={index}>
+                  <td className="date">{data.date}</td>
+                  <td className="views">{data.views}</td>
+                  <td className="article">{data.article}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
